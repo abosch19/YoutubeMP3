bc277e8dbd0ffeef674cd04a6b8821e3afb3680a
Alex Bosch <abosch19@gmail.com>
2016-01-05 21:27:10 +0100

    Utilitzacio de la Api de Youtubeinmp3, NOMÉS FALTA ICONO + DETALLAR INFO(manifest.json)

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+//Author: Alex Bosch
+//Github: https://www.github.com/abosch19
+//Project: https://github.com/abosch19/YoutubeMP3
+
 
 //------------------------------------------------------------------------------
 //Extension boton listener//
@@ -6,7 +10,7 @@ chrome.browserAction.onClicked.addListener(function () {
   chrome.tabs.getSelected(null,function (tab) {
     var tabLink = tab.url;
     if(tabLink.slice(0,32) == 'https://www.youtube.com/watch?v='){
-      downloadVideo(tabLink, tab);
+      downloadMusic(tabLink);
     }
     else {
       alert("ERROR");
@@ -16,51 +20,13 @@ chrome.browserAction.onClicked.addListener(function () {
 });
 
 //--------------------------------------------------------------------------------
-//Function downloads youtube video to mp3//
+//API
 //--------------------------------------------------------------------------------
+function downloadMusic(tabLink) {
+  var apiLink = "http://www.youtubeinmp3.com/fetch/?video="
+  var link = apiLink + tabLink;
+  chrome.tabs.update(null,{url:link});
+}
 
-function downloadVideo (tabLink, tab) {
-  chrome.tabs.create({url:'http://www.youtube-mp3.org/'}, function (tabYou) {
-    chrome.tabs.executeScript(null,{
-        code:'var url = "'+tabLink+'";'
-      }, function () {
-          chrome.tabs.executeScript(null,{
-            file:"js/script.js"
-          }, function () {
-              chrome.tabs.remove(tabYou.id, function () {
-              chrome.tabs.highlight({tabs: tab.index},function(){});
-            });
-        });
-      });
-
-    });
-  }
-    /*
-//-----------------------------------------------------------------------------------
-//onUpdate listener//
-//-----------------------------------------------------------------------------------
-
-chrome.tabs.onUpdated.addListener(function () {
-  chrome.tabs.getSelected(null,function(tab) {
-    var tabLink = tab.url;
-    if(tabLink.slice(0,32) == 'https://www.youtube.com/watch?v='){
-
-    }
-  });
-});
-
-//-----------------------------------------------------------------------------------
-//onCreated listener//
-//-----------------------------------------------------------------------------------
-
-chrome.tabs.onCreated.addListener(function () {
-  chrome.tabs.getSelected(null,function(tab) {
-    var tabLink = tab.url;
-    if(tabLink.slice(0,32) == 'https://www.youtube.com/watch?v='){
-    }
-    else {
-    }
-  });
-});
-*/
-
+//---------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------
